29c6a7c02dbe4e7d36776da0a7a8f4a24af14c80
Vetle <vetletb@stud.ntnu.no>
2023-10-23 16:07:24 +0200

    Lagt inn biblioteker til ev3 og lagd samlebånd objekt

---
+++ main.py
@@ -0,0 +1,13 @@
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
+
+ev3 = EV3Brick()
+samleBånd = Motor(Port.B)
+
+samleBånd.run(10)
+wait(10000)
