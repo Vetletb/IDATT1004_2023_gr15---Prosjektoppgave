343d8a263022000c7dc8fd36f34c9697ad652d45
Vetle <vetletb@stud.ntnu.no>
2023-11-03 11:16:52 +0100

    flyttet avfall, små rettelser

--- main.py
+++ main.py
@@ -13,15 +13,15 @@ beholder = Motor(Port.D)
 fargeSensor = ColorSensor(Port.S3)
 knapp = TouchSensor(Port.S2)
 
-avfallBeholder = 0
-rødBeholder = 1050
-blåBeholder = 2100
+rødBeholder = 0
+blåBeholder = 1050
+avfallBeholder = 2100
 grønnBeholder = 3150
 ukjentBeholder = 4200
 
-gjeldendeBeholder = rødBeholder
+gjeldendeBeholder = 0
 fart = 100
-sorteringTid = 5000
+sorteringTid = 1000
 
 while not knapp.pressed():
     continue
@@ -42,33 +42,36 @@ while not knapp.pressed():
                 else:
                     beholder.run_time(-fart, abs(gjeldendeBeholder - rødBeholder))
                 ev3.speaker.say("red")
-                wait(gjeldendeBeholder - rødBeholder)
+                wait(sorteringTid - (gjeldendeBeholder - rødBeholder))
                 gjeldendeBeholder = rødBeholder
+                print("rød")
             elif blå * 100 / sum >= 50 and blå > 3:
                 if gjeldendeBeholder - blåBeholder > 0:
                     beholder.run_time(fart, abs(gjeldendeBeholder - blåBeholder))
                 else:
                     beholder.run_time(-fart, abs(gjeldendeBeholder - blåBeholder))
                 ev3.speaker.say("blue")
-                wait(gjeldendeBeholder - blåBeholder)
+                wait(sorteringTid - (gjeldendeBeholder - blåBeholder))
                 gjeldendeBeholder = blåBeholder
+                print("blå")
             elif grønn * 100 / sum >= 50 and grønn > 3:
                 if gjeldendeBeholder - grønnBeholder > 0:
                     beholder.run_time(fart, abs(gjeldendeBeholder - grønnBeholder))
                 else:
                     beholder.run_time(-fart, abs(gjeldendeBeholder - grønnBeholder))
                 ev3.speaker.say("green")
-                wait(gjeldendeBeholder - grønnBeholder)
+                wait(sorteringTid - (gjeldendeBeholder - grønnBeholder))
                 gjeldendeBeholder = grønnBeholder
-            else: {
-                if gjeldendeBeholder - avfallBeholder > 0:
-                    beholder.run_time(fart, abs(gjeldendeBeholder - avfallBeholder))
-                else:
-                    beholder.run_time(-fart, abs(gjeldendeBeholder - avfallBeholder))
-                wait(sortereTid - (gjeldendeBeholder - avfallBeholder))
-                gjeldendeBeholder = avfallBeholder
-            }
+                print("grønn")
+        else:
+            if gjeldendeBeholder - avfallBeholder > 0:
+                beholder.run_time(fart, abs(gjeldendeBeholder - avfallBeholder))
+            else:
+                beholder.run_time(-fart, abs(gjeldendeBeholder - avfallBeholder))
+            gjeldendeBeholder = avfallBeholder
+            print("avfall")
+
         wait(250)
     beholder.run_time(fart, 4200)
-    gjeldendeBeholder = AvfallBeholder
+    gjeldendeBeholder = avfallBeholder
     wait(2000)
